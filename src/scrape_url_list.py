9c4ea31f8a241c5c950328a2029098d68f758074
MythiliPalanisamy <102500645+MythiliPalanisamy@users.noreply.github.com>
2023-11-02 06:23:38 +0100

    rearranging folders

--- src/scrape_url_list.py
+++ src/scrape_url_list.py
@@ -6,15 +6,8 @@ import requests
 from random import randint
 import time
 from time import sleep
-import json
 
-# Variables
-###########
-
-# html_text= requests.get('https://www.immoweb.be/en/classified/exceptional-property/for-sale/antwerp/2000/10591004').text  #property result used as example by Jonathan
 base_url = "https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE&orderBy=newest&page=" #starting point for all housing properties of Belgium, sorted starting from newest
-# "https://www.immoweb.be/en/search/house-and-apartment/for-sale/Theux/4910?countries=BE&page=1&orderBy=newest"   #4910 to have a pool of (47) but some oddball properties like castle, farmhouse and duplex
-# base_url = 'https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE&postalCodes=BE-4920,4910,4000&orderBy=newest&page='     #4910 & 4920 to have 5 result pages, 112 results
 page_url = base_url #will get a page NUMBER attached later, but this way page_url can be called already, immoweb does not mind the missing page number
 
 
@@ -31,12 +24,6 @@ def get_model(page_url):
 def get_property_list(site_model):
     """extract the urls of all search results from the page"""
     properties_per_page = site_model.xpath('//main/div//a[@class="card__title-link"]/@href') #creates a list of all search results (and excludes "similar properties" and "sponsored properties" )
-    #print(properties_per_page)
-    #print("items on this page: ", len(properties_per_page))
-    """unique_properties_per_page = list(set(properties_per_page)) #create a list of unique properties by creating a set and turning that into a list again
-    print(unique_properties_per_page)
-    print("items on this page: ", len(unique_properties_per_page))
-    properties_per_page = unique_properties_per_page"""
     return properties_per_page
 
 def page_list_to_full_list(full_list, properties_per_page):
@@ -48,15 +35,10 @@ def page_list_to_full_list(full_list, properties_per_page):
     print("list length: ",len(full_list))
     return full_list
 
-"""def add_to_file(properties_per_page):
-    with open('20k_belgium_url_list.json', 'a') as output_file:
-        json.dump(properties_per_page, output_file)"""
-
 def add_to_file(properties_per_page):
     with open('10k_belgium_url_list.txt', 'a') as output_file:
         for line in properties_per_page:
             output_file.write(f"{line}\n")
-
 
 def run_through_pages():
     """Scrapes property urls from search results page after page"""
@@ -75,7 +57,5 @@ def run_through_pages():
 
 start = time.time()
 final_list = run_through_pages()
-#print("final list:", final_list)
-#print("final list length: ",len(final_list))
 end = time.time()
 print("Time Taken: {:.6f}s".format(end-start))
